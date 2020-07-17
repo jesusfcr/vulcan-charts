3839c07ff725bbf24a66ac4870f609120d1d9d36
Jesus-Carpintero <jesus.carpintero@adevinta.com>
2020-07-17 14:21:57 +0200

    Use traditional method

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-set -ev
-
-eval $(ssh-agent)
-echo $ID_RSA | base64 -d | ssh-add
-
-git stash -u
-git config --global url.ssh://git@github.com/.insteadOf https://github.com/
-git checkout gh-pages
-mv -f $HOME/stable/*.tgz .
-=======
 eval $(ssh-agent -s)
 echo $ID_RSA | base64 -d | ssh-add -
 
@@ -24,7 +13,6 @@ git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 git checkout gh-pages
 
 mv -f $HOME/pages/*.tgz .
->>>>>>> build
 if [[ -z `git status -s` ]]; then
     echo "tree is clean"
 else
@@ -32,10 +20,5 @@ else
     helm repo index . --url https://adevinta.github.io/vulcan-charts
     git add .
     git commit -m "$TRAVIS_COMMIT_MESSAGE"
-<<<<<<< HEAD
-    # git push origin gh-pages
-fi      
-=======
     git push origin gh-pages
 fi
->>>>>>> build
